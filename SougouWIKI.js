61e66ff43b9b09245e085d868d21b76791cd54c4
YLaido <tbag781623489@gmail.com>
2018-05-26 20:34:25 +0800

    Bug fixes.

--- SougouWIKI.js
+++ SougouWIKI.js
@@ -16,15 +16,16 @@
         $.each(title,function(i,t){
             if (t.href.indexOf('www.dmm.co.jp') > -1) { //标题的A标签有可能会与图片的A标签重复，此处为判断机制
                 let num = /[a-zA-Z]{2,5}\-{0,1}\d{2,4}/;
-                let avmoo_ref = '<a href=' +"https://javmoo.net/cn/search/" + num.exec(t.href.split('=').pop())[0] +
-                    ' style="color:red;padding-left: 1.2em;font-weight: bold;" target="_blank">AVMOO</a>';
-                if (t.nextElementSibling && t.nextElementSibling.innerText === '(レーベル一覧)') {
-                    let series = t.nextElementSibling;
-                    $(series).after(avmoo_ref);  // 有系列一览
-                }
-                else{
-                    $(t).after(avmoo_ref);      // 无系列一览
-                }
+                if (num.exec(t.href.split('=').pop())) {
+                    let avmoo_ref = '<a href=' +"https://javlog.com/cn/search/" + num.exec(t.href.split('=').pop())[0] +
+                        ' style="color:red;padding-left: 1.2em;font-weight: bold;" target="_blank">AVMOO</a>';
+                    if (t.nextElementSibling && t.nextElementSibling.innerText === '(レーベル一覧)') {
+                        let series = t.nextElementSibling;
+                        $(series).after(avmoo_ref);  // 有系列一览
+                    }
+                    else{
+                        $(t).after(avmoo_ref);      // 无系列一览
+                    }}
             }});
     });
 })();
