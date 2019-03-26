781219cf2e68cdb65c8997044a9153601b02c255
YLaido <tbag781623489@gmail.com>
2019-03-26 18:53:01 +0800

    Better Experience on Search Page

--- javlibrary_to_avmoo.js
+++ javlibrary_to_avmoo.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         javlibrary
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.2
 // @description  try to take over the world!
 // @author       YLaido
 // @match        *://www.javlibrary.com/cn/?v=*
+// @match        *://www.javlibrary.com/cn/search.php
 // @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
 // @require      http://libs.baidu.com/jquery/2.0.0/jquery.js
 // @connect      avmoo.net
 // @connect      avmoo.xyz
@@ -25,6 +27,14 @@ function insertAfter(newNode, referenceNode) {
 }
 
 (function() {
+    GM_addStyle([".multiselectblock {height:550px;}"]);
+    if (location.pathname.indexOf("search.php") > -1) {
+        let now = new Date();
+        $("#start_year")[0].value = 2000;
+        $("#start_month")[0].value = 1;
+        $("#end_year")[0].value = now.getFullYear();
+        $("#end_month")[0].value = now.getMonth() +1;
+    }
     var parser = new DOMParser();
     var Serial = $('td.text')[0];
     var avmoJump = document.createElement('a');
